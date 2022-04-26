d3234cbe186e6630bef3d8722c09fa2e9e94616b
elopezcon <eduardo.contreras@wizelinecom>
2022-04-26 12:28:53 -0500

    Included items and user info in testData

--- e2e/data/testData.js
+++ e2e/data/testData.js
@@ -15,5 +15,22 @@ module.exports = {
         emptyField: '',
         invalidUser: 'test@test.com',
         invalidPassword: 'testcredentials'
+    },
+    items: {
+        backpack: 'Sauce Labs Backpack',
+        light: 'Sauce Labs Bike Light',
+        tShirt: 'Sauce Labs Bolt T-Shirt',
+        fleece: 'Sauce Labs Fleece Jacket',
+
+    },
+    user: {
+        name: 'Homero Sipmson',
+        address: 'Avenida Siempreviva No 712',
+        city: 'Springfield',
+        zip: 89758,
+        country: 'US',
+        cardNumber: '4111111111111111',
+        expirationDate: 1227,
+        securityCode: 333
     }
 }
